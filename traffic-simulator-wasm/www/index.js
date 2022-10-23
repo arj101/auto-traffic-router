c154ba57ad1fe44a5d232731b056fe16698d759d
arduino explorer <arjunanmr101@gmail.com>
2022-10-23 19:43:45 +0530

    add www folder

---
+++ traffic-simulator-wasm/www/index.js
@@ -0,0 +1,29 @@
+import * as wasm from "traffic-simulator-wasm";
+
+wasm.greet()
+
+const canvas = document.getElementsByTagName('canvas')[0]
+canvas.width = window.innerWidth - 5;
+canvas.height = window.innerHeight - 5;
+
+window.onresize = () => {
+    canvas.width = window.innerWidth - 5;
+    canvas.height = window.innerHeight - 5;
+}
+
+const ctx = canvas.getContext('2d')
+
+
+const renderLoop = () => {
+    requestAnimationFrame(renderLoop)
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = 'rgb(30, 30,30)'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.fill()
+    ctx.fillStyle = 'rgba(10, 10, 10, 0.5)'
+    ctx.arc(canvas.width / 2, canvas.height / 2, 5, 0, 2 * Math.PI)
+    ctx.fill()
+}
+
+renderLoop()
