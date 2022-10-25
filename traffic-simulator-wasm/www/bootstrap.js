ed013f770a58e7af6c98f7eeed0a69788305a430
arduino explorer <arjunanmr101@gmail.com>
2022-10-25 16:16:53 +0530

    properly load map-data.js

--- traffic-simulator-wasm/www/bootstrap.js
+++ traffic-simulator-wasm/www/bootstrap.js
@@ -1,6 +1,7 @@
 // A dependency graph that contains any wasm must all be imported
 // asynchronously. This `bootstrap.js` file does the single async import, so
 // that no one else needs to worry about it again.
-import("./map-data");
-import("./index.js")
-  .catch(e => console.error("Error importing `index.js`:", e));
+import("./map-data.js").then(() => {
+  import("./index.js")
+    .catch(e => console.error("Error importing `index.js`:", e));
+}).catch(e => console.error("Error importing `map-data.js`:", e))
