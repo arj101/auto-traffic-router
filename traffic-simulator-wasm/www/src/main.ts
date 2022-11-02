db8ee3ab7cfa01bfef732d813d5e3ef3cfe72fb0
arj101 <arjunanmr101@gmail.com>
2022-11-02 19:33:38 +0530

    making it clear that this is for science fair 2022

--- traffic-simulator-wasm/www/src/main.ts
+++ traffic-simulator-wasm/www/src/main.ts
@@ -3,6 +3,9 @@ import App from "./App.svelte";
 
 const app = new App({
     target: document.body,
+    props: {
+        itemCode: import.meta.env.VITE_ITEM_CODE || '<no item code found>'
+    }
 });
 
 export default app;
